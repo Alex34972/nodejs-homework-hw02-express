54f6a3bc500c9e417968f3c61622ace10f8a8c93
Alex34972 <alex_hifi@ukr.net>
2021-10-22 20:41:30 +0300

    +++=

--- config/constants.js
+++ config/constants.js
@@ -6,8 +6,11 @@ const ValidName = {
 const StatusCode = {
   OK: 200,
   CREATED: 201,
+  NO_CONTENT: 204,
   BAD_REQUEST: 400,
+  UN_AUTHORIZED:401,
   NOT_FOUND: 404,
+  CONFLICT: 409,
   SERVER_ERROR: 500,
 }
 
---
+++ controllers/users.js
@@ -0,0 +1,69 @@
+const Users = require('../repository/users')
+const {StatusCode} = require('../config/constants')
+
+
+const signup = async (req, res, next) => {
+
+    const {email} = req.body
+    const user = await Users.findByEmail(email)
+    if (user) {
+        return res.status(StatusCode.CONFLICT).json({
+            status: 'Error',
+            code: StatusCode.CONFLICT,
+            message: 'Email is already exist'
+        })
+    }
+    try{
+    const newUser = await Users.create(req.body)
+        return res.status(StatusCode.CREATED).json({
+          status:'Success',
+          code:StatusCode.CREATED,
+          data:{
+             id: newUser.id,
+             name: newUser.name,
+             email: newUser.email
+           }
+        })
+  }catch (error){
+    next(error)
+  }
+}
+const login = async (req, res) => {
+    try {
+      const { email, password } = req.body
+      const user = await Users.findByEmail(email)
+      const isValidPassword = await user.isValidPassword(password)
+      if (!user || !isValidPassword){
+        return res.status(StatusCode.BAD_REQUEST).json({
+          status: 'Error',
+          code:StatusCode.BAD_REQUEST,
+          message: 'Email or password is wrong',
+        })
+      }
+      const id = user.id
+      const payload = { id }
+      const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '5h' })
+      await Users.updateToken(id, token)
+      return res.status(StatusCode.OK).json({
+        status:'Success',
+        code:StatusCode.OK,
+        data:{token}
+      })
+        
+    } catch(error){
+      return res.status(StatusCode.UN_AUTHORIZED).json({
+        status: 'Error',
+        code:StatusCode.UN_AUTHORIZED,
+        message:'Invalid credentials'
+      })
+
+    }
+  }
+
+const logout = async(rec,res) => {
+  const id = req.user.id
+  await Users.updateToken(id, null)
+  return res.status(StatusCode.NO_CONTENT).json({})
+}
+
+module.exports = {signup,login,logout}
