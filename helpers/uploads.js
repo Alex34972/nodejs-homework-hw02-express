88a8163079ca2c03d6205cf85ee8956522bc085a
Alex34972 <alex_hifi@ukr.net>
2021-10-30 20:34:20 +0300

    Create uploads.js

---
+++ helpers/uploads.js
@@ -0,0 +1,29 @@
+
+const multer  = require('multer')
+require('dotenv').config()
+const { StatusCode } = require('../config/constants')
+const UPLOAD_DIR = process.env.UPLOAD_DIR
+
+
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    cb(null, UPLOAD_DIR)
+  },
+  filename: function (_req, file, cb) {
+    cb(null, `${Date.now().toString()}_${file.originalname}`)
+  },
+})
+  const upload = multer({
+    storage: storage,
+    limits: {filedSize:10000000},
+    fileFilter: (_req, file, cb) => {
+      if (file.mimetype.includes('image')) {
+        return cb(null, true)
+      }
+      const err = new Error('not picture')
+      err.status = StatusCode.BAD_REQUEST
+      cb(err)
+  
+  } })
+
+module.exports = upload
