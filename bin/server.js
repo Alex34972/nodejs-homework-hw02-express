75ff290209e1ed188bd900c4140dd490365d89c3
Alex34972 <alex_hifi@ukr.net>
2021-10-30 20:33:39 +0300

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,16 @@
 const db = require('../config/db')
 const app = require('../app')
+require('dotenv').config()
+const UPLOAD_DIR = process.env.UPLOAD_DIR
+const AVATAR_USERS = process.env.AVATAR_USERS
+const mkdirp = require('mkdirp')
 
 const PORT = process.env.PORT || 8000
 
 db.then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT,async() => {
+    await mkdirp(UPLOAD_DIR)
+    await mkdirp(AVATAR_USERS)
     console.log(`Server running. Use our API on port: ${PORT}`)
   })
 }).catch((err) => {
