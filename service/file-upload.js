79354bb972fb455ea0299a4b2925cca5ad7a3117
Alex34972 <alex_hifi@ukr.net>
2021-10-31 22:02:29 +0200

    Update file-upload.js

--- service/file-upload.js
+++ service/file-upload.js
@@ -3,23 +3,22 @@ const Jimp = require('jimp')
 const path = require('path')
 
 class uploadFileAvatar {
-    constructor(dest) {
-        this.dest = dest
-    }
-    async #transformAvatar(pathFile) {
-        const picture = Jimp.read(pathFile)
-        await (await picture).autocrop()
-            .cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE)
-            .writeAsync(pathFile)
-    }
-    async save( file, idUser ) {
-        await this.#transformAvatar(file.path)
-        await fs.rename(file.path, path.join(this.dest, file.filename))
-        return path.normalize(path.join(idUser, file.filename))
+  constructor(dest) {
+    this.dest = dest
+  }
 
-    }
+  async transformAvatar(pathFile) {
+    const picture = Jimp.read(pathFile)
+    await (await picture).autocrop()
+      .cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE)
+      .writeAsync(pathFile)
+  }
 
+  async save(file, idUser) {
+    await this.transformAvatar(file.path)
+    await fs.rename(file.path, path.join(this.dest, file.filename))
+    return path.normalize(path.join(idUser, file.filename))
+  }
 }
-
 
 module.exports = uploadFileAvatar
