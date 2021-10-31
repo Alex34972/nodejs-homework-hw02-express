0ad63084a223a2ac49614bf0b57f5ddad4a143a1
Alex34972 <alex_hifi@ukr.net>
2021-10-31 22:02:26 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { validateCreateUser, validateLogin } = require('../../validation/validationUsers')
-const { signup, login, logout,uploadAvatar, current } = require('../../controllers/users')
+const { signup, login, logout, uploadAvatar, current } = require('../../controllers/users')
 const guard = require('../../helpers/guard')
 const loginLimiter = require('../../helpers/rateLimitLogin')
 const upload = require('../../helpers/uploads')
@@ -10,6 +10,6 @@ router.post('/registration', validateCreateUser, signup)
 router.post('/login', validateLogin, loginLimiter, login)
 router.post('/logout', guard, logout)
 router.get('/current', guard, current)
-router.patch('/avatar', guard, upload.single('avatarURL'),uploadAvatar)
+router.patch('/avatar', guard, upload.single('avatarURL'), uploadAvatar)
 
 module.exports = router
