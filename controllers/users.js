9ce244883dce3f2ff83357d20a53563ef17a3eee
Alex34972 <alex_hifi@ukr.net>
2021-10-31 21:37:41 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const path = require('path')
 const mkdirp = require('mkdirp')
 const Users = require('../repository/users')
 const { StatusCode } = require('../config/constants')
-const  UploadService  = require('../service/file-upload')
+const UploadService = require('../service/file-upload')
 require('dotenv').config()
 const SECRET_KEY = process.env.JWT_SECRET_KEY
 
@@ -70,44 +70,51 @@ const logout = async (req, res) => {
   return res.status(StatusCode.NO_CONTENT).json({})
 }
 
-const uploadAvatar =  async (req, res) => {
-  const id = String(req.user._id)
- const file = req.file
- const AVATAR_USERS = process.env.AVATAR_USERS
- const dest = path.join(AVATAR_USERS, id)
- await mkdirp(dest)
- const uploadService = new UploadService(dest)
- const avatarURL = await uploadService.save(file, id)
- await Users.updateAvatar(id, avatarURL)
- 
-  return res.status(StatusCode.OK).json({
-    status:'success',
-    code:StatusCode.OK,
-    date:{
-      avatarURL
-    }
-  })
-}
-
-
-
-
-const current = async (req, res, next) => {
+const uploadAvatar = async (req, res) => {
   try {
+  const id = String(req.user._id)
+  const file = req.file
+  const AVATAR_USERS = process.env.AVATAR_USERS
+  const dest = path.join(AVATAR_USERS, id)
+  await mkdirp(dest)
+  const uploadService = new UploadService(dest)
+    const avatarURL = await uploadService.save(file, id)
+    await Users.updateAvatar(id, avatarURL)
+
+    return res.status(StatusCode.OK).json({
+      status: 'success',
+      code: StatusCode.OK,
+      date: {
+        avatarURL
+      }
+    })
+  } catch (error) {
+    return res.status(StatusCode.UN_AUTHORIZED).json({
+      status: 'Error',
+      code: StatusCode.UN_AUTHORIZED,
+      message: 'Invalid credentials'
+    })
+  }}
+
+
+
+
+  const current = async (req, res, next) => {
+    try {
       const { id, name, email, subscription } = req.user;
       return res.status(StatusCode.OK).json({
-          status: 'Success',
-          code: StatusCode.OK,
-          data: {
-              id,
-              name,
-              email,
-              subscription,
-          },
+        status: 'Success',
+        code: StatusCode.OK,
+        data: {
+          id,
+          name,
+          email,
+          subscription,
+        },
       });
-  } catch (error) {
+    } catch (error) {
       next(error);
-  };
-}
+    };
+  }
 
-module.exports = { signup, login, logout, uploadAvatar, current }
+  module.exports = { signup, login, logout, uploadAvatar, current }
