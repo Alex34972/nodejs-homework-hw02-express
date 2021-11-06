db15df449595e44ffc90fd4a5ba249c94e75c626
Alex34972 <alex_hifi@ukr.net>
2021-11-06 12:27:37 +0200

    Update sender.js

--- service/email/sender.js
+++ service/email/sender.js
@@ -2,14 +2,12 @@ const sgMail = require('@sendgrid/mail')
 require('dotenv').config()
 
 class CreateSenderSendGrid {
-    async send(message) {
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-        return await sgMail
-        .send({ ...message, from: 'terry_cat@meta.ua'})
-        .then(() => {
-            console.log(message)
-          })
-    }
+  async send(message) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    return await sgMail
+      .send({ ...message, from: 'terry_cat@meta.ua' })
+     
+  }
 }
 
-module.exports = CreateSenderSendGrid 
+module.exports = CreateSenderSendGrid
