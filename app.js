aea6ba10164ea68f16154b9b4bf27cf0302f6a5b
Alexandr Petrenko <alex_hifi@ukr.net>
2021-11-13 14:14:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors())
 app.use(express.json())
 app.use(boolParser())
 
-app.use('/api/users', usersRouter)
+app.use('/api/users',   usersRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((_req, res) => {
